5a25fb00f2784cfc3586316eac567a4b933eb242
ojoo <ojoo_o@naver.com>
2020-04-19 02:09:52 +0900

    final

--- week4/indeed.py
+++ week4/indeed.py
@@ -74,16 +74,3 @@ if __name__ == "__main__":
     s.scrap()
 
 
-
-    def SepInfo(self, tds):
-         
-        weatherinfo = []
-
-        for td in tds:                
-            if td.find('a'):             
-                point = td.find('a').text
-                temperature = tds[5].text
-                humidity = tds[9].text      
-                weatherinfo.append([point, temperature, humidity])
-
-        return weatherinfo
--- week4/weather.py
+++ week4/weather.py
@@ -1,4 +1,6 @@
 
+# 기상청 홈페이지의 '도시별 현재 날씨' 자료에서 지역명, 기온, 습도, 풍향 데이터 크롤링 해오기
+
 
 import requests                
 from bs4 import BeautifulSoup  
@@ -18,15 +20,15 @@ class WeatherScraper() :
     def getWeatherInfo(self, soup):
         weathertable = soup.find('table', class_ = 'table_develop3' )
         weatherinfo=[]
-        for tr in weathertable.find_all('tr'):
-            tds = list(tr.find_all('td'))
+        for tr in weathertable.find_all('tr'): # 모든 tr 태그 찾기
+            tds = list(tr.find_all('td')) # 모든 td 태그 찾기
             for td in tds:                
                 if td.find('a'):             
-                    Point = td.find('a').text
-                    Temperature = tds[5].text
-                    Humidity = tds[10].text
-                    WindDirection = tds[11].text      
-                    weatherinfo.append([Point, Temperature, Humidity, WindDirection])
+                    Point = td.find('a').text # 지역명
+                    Temperature = tds[5].text # 기온
+                    Humidity = tds[10].text # 습도
+                    WindDirection = tds[11].text  # 풍향    
+                    weatherinfo.append([Point, Temperature, Humidity, WindDirection]) # 날씨정보리스트에 추가
 
         return weatherinfo
     
