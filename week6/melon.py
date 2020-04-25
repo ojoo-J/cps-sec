cd352ce577b8653a249a5b4f3af178f24480994c
ojoo <ojoo_o@naver.com>
2020-04-25 17:36:45 +0900

    final

--- week6/melon.py
+++ week6/melon.py
@@ -5,6 +5,9 @@ import os
 import time
 
 
+## 멜론에서 곡명으로 검색하기 ##
+
+
 path = os.getcwd() + "/week6/chromedriver.exe"
 driver = webdriver.Chrome(path)
 
@@ -12,22 +15,22 @@ try:
     driver.get("https://www.melon.com/index.htm")
     time.sleep(1)
 
-    searchIndex = "사랑" #검색할 내용
+    searchIndex = "사랑" #검색할 키워드
 
     element = driver.find_element_by_xpath('//*[@id="top_search"]') #검색창 찾기
-    element.send_keys(searchIndex) #searchIndex를 검색하자!
+    element.send_keys(searchIndex) #searchIndex를 검색어로 전송
     driver.find_element_by_class_name("btn_icon.search_m").click() #검색버튼 찾고 클릭
     time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="divCollection"]/ul/li[3]/a').click()
+    driver.find_element_by_xpath('//*[@id="divCollection"]/ul/li[3]/a').click() # '곡명으로 검색' 클릭
     
     
     title = []
-    for i in range(3): 
+    for i in range(3): # 3페이지 까지만
         time.sleep(1)
 
         html = driver.page_source
         bs = BeautifulSoup(html, "html.parser")
-        conts = bs.find_all("table")[0].find("tbody").find_all("tr") 
+        conts = bs.find_all("table")[0].find("tbody").find_all("tr") # table -> tbody -> tr
 
         title.append("\n<<<<page"+str(i+1)+">>>>") #페이지 번호 출력
 
